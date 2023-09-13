e7c71a7e9a2c9be9709aa0fbdb7c2abf5744a20a
benjaminfisico <benjaminfisico@gmail.com>
2023-09-13 12:02:56 -0300

    Change LOG: - Ahora el password por defecto es "12345678" para facilitar la prueba del sistema. - Ahora los seeders tambien agregan un rol de forma aleatoria para cada usuario. - Se creo un usuario administrador por defecto con email: benjaminfisico@gmail.com  y constraseña: 12345678

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(100)->create();
+        User::factory()->create([
+            'name' => 'Benjamin Fisico',
+            'email' => 'benjaminfisico@gmail.com',
+            'role' => 'admin'
+        ]);
+        User::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
